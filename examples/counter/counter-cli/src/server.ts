7e2575fa9d486eb2effe0a0ebadf764ec7f7a595
Dilmurod Yangiboev <dilmurod.yangiboev@gmail.com>
2024-07-27 14:26:16 +0200

    apis to create wallet & contract

--- examples/counter/counter-cli/src/server.ts
+++ examples/counter/counter-cli/src/server.ts
@@ -4,7 +4,11 @@ import path from 'path';
 import { createLogger } from './logger-utils.js';
 import { DevnetRemoteConfig } from './config.js';
 import * as api from './api.js';
+import { toHex } from './conversion-utils.js';
+
 export const currentDir = path.resolve(new URL(import.meta.url).pathname, '..');
+import * as Rx from 'rxjs';
+import { send } from 'process';
 
 
 const app = express();
@@ -27,9 +31,46 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/seed/:seed', (req: Request, res: Response) => {
     const {seed} = req.params
-    console.log(seed)
+    logger.info(seed)
     res.sendFile(path.resolve(currentDir, '..', 'public'));
 });
+
+app.get('/add-wallet', async (req: Request, res: Response) => {
+    const seed = toHex(api.randomBytes(32))
+    const wallet = await api.buildWalletAndWaitForFunds(config, seed);
+    wallet.start();
+    const state = await Rx.firstValueFrom(wallet.state());
+    logger.info(`Your wallet seed is: ${seed}`);
+    logger.info(`Your wallet address is: ${state.address}`);
+
+    res.send({
+        "seed": seed,
+        "address": state.address,
+    })
+});
+
+app.get('/seed/:seed/submit-sales', async (req: Request, res: Response) => {
+    const {seed} = req.params
+    const {  amount } = req.body;
+    logger.info("Amount to sell: ", amount)
+    logger.info("Seed to get wallet: ", seed)
+    const wallet = await api.buildWalletAndWaitForFunds(config, seed);
+    wallet.start();
+    
+    const providers = await api.configureProviders(wallet, config);
+    const counterContract = await api.deploy(providers)
+    logger.info(`Deployed contract at address: ${counterContract.finalizedDeployTxData.contractAddress}`);
+    
+    const { txHash, blockHeight } = await api.increment(counterContract);
+
+    res.send({
+        "contract_address": counterContract.finalizedDeployTxData.contractAddress,
+        "transaction_hash": txHash,
+        "block_height": blockHeight,
+    })
+
+})
+
 
 interface FormData {
     seed: string;
@@ -37,13 +78,13 @@ interface FormData {
 
 app.post('/submit-seed', async (req: Request, res: Response) => {
     const {  seed }: FormData = req.body;
-    console.log('Form submission received:');
-    console.log('Name:', seed);
+    logger.info('Form submission received:');
+    logger.info('Name:', seed);
 
-    res.redirect(`/seed/${seed}`)
+    res.send({"wallet": "123"})
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    logger.info(`Server running at http://localhost:${port}`);
 });
